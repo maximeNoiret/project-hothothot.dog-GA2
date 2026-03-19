d3b1dd8283b288497e24d1181da32fd18a3cb138
adam-KUROPATWA-BUTTE <adam.kuropatwa-butte@etu.univ-amu.fr>
2026-03-19 09:34:16 +0100

    init: Initial Commit

---
+++ TP2/TP2.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>TP2</title>
+        <link rel="stylesheet" href="ressources/styles/style.css">
+    </head>
+    <body>
+        <header>
+            <h1>
+                Systeme de temperature
+            </h1>
+
+            <!-- Système d'onglets accessible avec ARIA -->
+            <div role="tablist" aria-label="Panneaux de température">
+                <button role="tab"
+                        id="btnTempsReel"
+                        class="active"
+                        aria-selected="true"
+                        aria-controls="tempsReel"
+                        tabindex="0">
+                    Temps Réel
+                </button>
+
+                <button role="tab"
+                        id="btnHistorique"
+                        aria-selected="false"
+                        aria-controls="historique"
+                        tabindex="-1">
+                    Historique
+                </button>
+            </div>
+            <hr>
+        </header>
+        <main>
+            <!-- Panneau 1: Temps Réel -->
+            <div id="tempsReel"
+                 role="tabpanel"
+                 aria-labelledby="btnTempsReel"
+                 tabindex="0">
+                <h2>
+                    Temperature:
+                </h2>
+                <p id="temperature" aria-live="polite" aria-atomic="true" aria-relevant="all"></p>
+                <div id="avertTemp" role="alert">
+                    <p id="message" aria-live="polite" aria-atomic="true" aria-relevant="all"></p>
+                </div>
+            </div>
+
+            <!-- Panneau 2: Historique -->
+            <div id="historique"
+                 role="tabpanel"
+                 aria-labelledby="btnHistorique"
+                 tabindex="0"
+                 class="hidden"
+                 hidden>
+                <h2>
+                    Historique des temperatures:
+                </h2>
+                <p id="listeHistorique" aria-live="polite" aria-atomic="true" aria-relevant="all">
+
+                </p>
+            </div>
+        </main>
+        <script src="ressources/scripts/main.js"></script>
+    </body>
+</html>
